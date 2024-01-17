f48fb37c313a97598954eebd28063dcb3d9139f8
tomartin <tomartin@student.42madrid.com>
2024-01-17 14:44:04 +0100

    end ft__strdup

--- ft__strdup.s
+++ ft__strdup.s
@@ -12,10 +12,20 @@ extern malloc
 
 ft__strdup:
 	xor  rax, rax
-	mov  r9, rdi
+	;mov r9, rdi
+	push rdi
 	call ft__strlen
 	inc  rax
 	mov  rdi, rax
 	call malloc
+	cmp  rax, 0
+	je   end_fail
+	pop  rsi
+	mov  rdi, rax
+	call ft__strcpy
+	ret
 
+end_fail:
+	xor rax, rax
+	mov rdi, 0
 	ret
--- test.c
+++ test.c
@@ -14,41 +14,43 @@ int ft__strcmp(const char *str1, const char *str2);
 char *ft__strcpy(char *restrict dst, const char *restrict src);
 char *ft__strdup(const char *str);
 void strdup_test(const char *s) {
-  printf("%s -- %p\n", s, s);
   char *res = ft__strdup(s);
-  printf("%s - %p ===  \n", s, res);
+  printf("%s - %p === %s - %p \n", s, s, res, res);
   free(res);
-  printf("%s - %p ===  \n", s, s);
 }
 
 int main(void) {
 
   char buff[20];
 
-  //  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRLEN");
-  //  STRLEN("HOLA");
-  //  STRLEN("");
-  //  STRLEN(" ");
-  //  printf("\n");
-  //  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRCMP");
-  //  STRCMP("HOLA", "HOLA");
-  //  STRCMP("HOLA1", "HOLA");
-  //  STRCMP("HOLA", "HOLA1");
-  //  STRCMP("HOLA", "ADIOS");
-  //  STRCMP("42", "");
-  //  STRCMP("", "42");
-  //  STRCMP("", "");
-  //  STRCMP(" ", "");
-  //  STRCMP("", " ");
-  //  printf("\n");
-  //  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRCPY");
-  //  printf("('HOLA') %s === %s\n", strcpy(buff, "HOLA"),
-  //         ft__strcpy(buff, "HOLA"));
-  //  printf("('') %s === %s\n", strcpy(buff, ""), ft__strcpy(buff, ""));
-  //  printf("('AAAAAAAAAAAAAAAAAA') %s === %s\n",
-  //         strcpy(buff, "AAAAAAAAAAAAAAAAAA"),
-  //         ft__strcpy(buff, "AAAAAAAAAAAAAAAAAA"));
-  //  printf("\n");
-  //  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRDUP");
+  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRLEN");
+  STRLEN("HOLA");
+  STRLEN("");
+  STRLEN(" ");
+  printf("\n");
+  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRCMP");
+  STRCMP("HOLA", "HOLA");
+  STRCMP("HOLA1", "HOLA");
+  STRCMP("HOLA", "HOLA1");
+  STRCMP("HOLA", "ADIOS");
+  STRCMP("42", "");
+  STRCMP("", "42");
+  STRCMP("", "");
+  STRCMP(" ", "");
+  STRCMP("", " ");
+  printf("\n");
+  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRCPY");
+  printf("('HOLA') %s === %s\n", strcpy(buff, "HOLA"),
+         ft__strcpy(buff, "HOLA"));
+  printf("('') %s === %s\n", strcpy(buff, ""), ft__strcpy(buff, ""));
+  printf("('AAAAAAAAAAAAAAAAAA') %s === %s\n",
+         strcpy(buff, "AAAAAAAAAAAAAAAAAA"),
+         ft__strcpy(buff, "AAAAAAAAAAAAAAAAAA"));
+  printf("\n");
+  printf("\n\033[1mTesting: \033[0m\033[35m%s\033[0m...\n", "STRDUP");
   strdup_test("hola");
+  strdup_test("");
+  strdup_test("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+  strdup_test(" ");
+  strdup_test("\n");
 }
